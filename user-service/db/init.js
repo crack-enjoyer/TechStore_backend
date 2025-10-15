7d0ee548ab618c9a178b52fea675cf5ebd66c6e4
leflop <petertsoy05@mail.ru>
2025-10-15 15:01:15 +0300

    Заданы модели и конфиг для микросервиса пользователей

---
+++ user-service/db/init.js
@@ -0,0 +1,32 @@
+const { Sequelize } = require('sequelize');
+const config = require('../config/config');
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    port: config.port,
+    dialect: config.dialect,
+    logging: config.logging || false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    define: {
+      underscored: false,
+      freezeTableName: true, 
+    }
+  }
+);
+
+const User = require('./models/User')(sequelize);
+
+module.exports = {
+  sequelize,
+  Sequelize,
+  User,
+};
